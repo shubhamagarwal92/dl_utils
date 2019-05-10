0d977d1bea8e43f92c0d369c5ce4115b5cfa3902
Shubham Agarwal <shubhamagarwal92@gmail.com>
2019-05-10 15:02:50 +0100

    cuda nvcc

--- starter.sh
+++ starter.sh
@@ -4,3 +4,6 @@ wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 sudo apt-get install tmux
 # Install htop
 sudo apt-get install htop
+# Install cuda
+sudo apt install nvidia-cuda-toolkit
+
