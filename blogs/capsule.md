33f02b8bae5b4f8dec24ae61000554214b210a49
Shubham Agarwal <shubhamagarwal92@gmail.com>
2019-05-10 13:55:22 +0100

    SA: starter sh

---
+++ blogs/capsule.md
@@ -0,0 +1,1 @@
+https://github.com/cezannec/capsule_net_pytorch/blob/master/Capsule_Network.ipynb
---
+++ starter.sh
@@ -0,0 +1,6 @@
+# Install conda
+wget https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh
+# Install tmux
+sudo apt-get install tmux
+# Install htop
+sudo apt-get install htop
