c814538237e5d2335a712c5227b97e4e322bdce7
Shubham Agarwal <shubhamagarwal92@gmail.com>
2025-09-01 20:45:17 +0530

    Add top-p sampling function for logits
    Implemented top-p (nucleus) sampling function for logits with optional searchsorted optimization.

--- pytorch_related/gen_sampling.py
+++ pytorch_related/gen_sampling.py
@@ -25,6 +25,53 @@ def top_k_sampling(logits, k=50, temperature=1.0):
   # Also need to "gather" / map it to original indices (over Vocab). 
   # Top k changed the order earlier but preserved the mapping
   
-
+# ----------- Top-p (Nucleus) Sampling -----------
+# See the searchsorted one that is more optimized on PyTorch
+def top_p_sampling(logits, p=0.9, temperature=1.0, use_searchsorted=True):
+  # Sort logits
+  sorted_logits, sorted_indices = torch.sort(logits, descending=True, dim=-1)
+  sorted_logits = sorted_logits / temperature
+  probs = F.softmax(sorted_logits, dim=-1)
+  if use_searchsorted:
+    # Efficient cutoff detection
+    cumulative_probs = torch.cumsum(probs, dim=-1)
+    # cutoff = torch.searchsorted(cumulative_probs, p, right=True)
+    # cumulative_probs: [batch, vocab]
+    # Makes a tensor of shape [batch, 1]
+    # Fills every element with the scalar value p.
+    p_tensor = torch.full((cumulative_probs.size(0), 1), p, device=logits.device)
+    cutoff = torch.searchsorted(cumulative_probs, p_tensor, right=True).squeeze(-1) # [batch_size]
+  
+    batch_size, vocab_size = logits.shape
+    arange = torch.arange(vocab_size, device=logits.device).unsqueeze(0).expand(batch_size, -1)  # (batch_size, vocab_size)
+    # print(arange)
+    cutoff_expanded = cutoff.unsqueeze(1).expand_as(arange) # [batch_size, vocab_size]
+    # print(cutoff_expanded)
+    mask = arange > cutoff_expanded
+    """
+    Eg: batch_size = 2; vocab_size = 6
+    arange = [[0, 1, 2, 3, 4, 5],
+      [0, 1, 2, 3, 4, 5]]  # shape [2, 6]
+    cutoff_expanded = [[3, 3, 3, 3, 3, 3],
+               [4, 4, 4, 4, 4, 4]]  # shape [2, 6]
+    mask = [[False, False, False, False, True, True],
+    [False, False, False, False, False, True]]
+    """
+  else:
+    # Simpler mask-shift version
+    cumulative_probs = torch.cumsum(probs, dim=-1)
+    mask = cumulative_probs > p
+    mask[..., 1:] = mask[..., :-1].clone()  # shift right
+    mask[..., 0] = False
+  
+  # Mask logits & re-normalize
+  filtered_logits = sorted_logits.masked_fill(mask, float('-inf'))
+  filtered_probs = F.softmax(filtered_logits, dim=-1)
+  
+  # Sample
+  sampled_idx = torch.multinomial(filtered_probs, num_samples=1)
+  return torch.gather(sorted_indices, -1, sampled_idx)
+  
+  
   
   
